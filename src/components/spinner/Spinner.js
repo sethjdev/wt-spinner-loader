e161c23e1e42c313a453824779279a97c7341fcc
Seth Johnson <sethjdesigns@gmail.com>
2020-07-18 18:03:06 -0400

    spinner styling

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import propTypes from "prop-types";
 import styles from "./Spinner.module.css";
 
 function Spinner(props) {
   const { started, progress, complete } = props;
-
+  const spinSvgRef = useRef(null);
   const circleWidth = 50;
-  const strokeDasharray = 2 * 3.14 * circleWidth;
-  const strokeDashoffset = strokeDasharray - strokeDasharray * (progress / 100);
+
+  const strokeDasharray = Math.PI * (2 * circleWidth) - 30;
+  let strokeDashoffset = ((100 - progress) / 100) * strokeDasharray;
+
+  if (!!spinSvgRef.current) {
+    if (started) {
+      spinSvgRef.current.style.animationPlayState = "running";
+    } else {
+      spinSvgRef.current.style.animationPlayState = "paused";
+    }
+  }
 
   return (
     <div className={styles.wrapper}>
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        className={styles.spinAnim}
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        ref={spinSvgRef}
+      >
         <circle
           cx="50"
           cy="50"
@@ -22,6 +36,22 @@ function Spinner(props) {
           }}
         />
       </svg>
+      <svg
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        className={styles.circleBg}
+      >
+        <circle
+          cx="50"
+          cy="50"
+          r={circleWidth - 5}
+          className={styles.circleTrack}
+        />
+      </svg>
+      <div className={styles.percentWrapper}>
+        <span className={styles.percentText}>{progress}</span>
+        <span className={styles.percentUnit}>%</span>
+      </div>
     </div>
   );
 }
