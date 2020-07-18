394be6a8521c5586657aaa5e7f2be1d9d4c8549a
Seth Johnson <sethjdesigns@gmail.com>
2020-07-17 23:19:39 -0400

    add prop types

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,10 +1,18 @@
 import React from "react";
 import style from "./Button.module.css";
+import PropTypes from "prop-types";
 
-export default ({ label, onClick }) => {
+function Button({ label, onClick }) {
   return (
     <button className={style.wrapper} onClick={onClick}>
       {label}
     </button>
   );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  label: PropTypes.string,
 };
+
+export default Button;
--- src/components/loader-card/LoaderCard.js
+++ src/components/loader-card/LoaderCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./LoaderCard.module.css";
 
-export default (props) => {
-  return <div className={styles.wrapper}>
-    
-  </div>;
-};
+function LoaderCard() {
+  return <div className={styles.wrapper}></div>;
+}
+
+export default LoaderCard;
